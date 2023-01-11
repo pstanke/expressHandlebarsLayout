43e277143a49bb03fde95313e5c3f7f209ba8e40
Pawel Stanke <pablostanke21@gmail.com>
2023-01-11 19:52:08 +0100

    Project done

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const path = require('path');
+const hbs = require('express-handlebars');
+
+const app = express();
+
+app.engine(
+  'hbs',
+  hbs({ extname: 'hbs', layoutsDir: './views/layouts', defaultLayout: 'main' })
+);
+app.set('view engine', '.hbs');
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('/hello/:name', (req, res) => {
+  res.render('hello', { layout: false, name: req.params.name });
+});
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact');
+});
+
+app.get('/info', (req, res) => {
+  res.render('info');
+});
+
+app.get('/history', (req, res) => {
+  res.render('history', { layout: 'dark' });
+});
+
+app.use((req, res) => {
+  res.status(404).send('404 not found...');
+});
+
+app.listen(8000, () => {
+  console.log('Server is running on port: 8000');
+});
