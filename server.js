a8b13450e15a09ec3ba3b1ba412adc7e6fae2e88
Pawel Stanke <pablostanke21@gmail.com>
2023-01-15 13:54:36 +0100

    Improve contact subpage

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('express-handlebars');
-
+const formidableMiddleware = require('express-formidable');
+const accepts = require('accepts');
 const app = express();
 
 app.engine(
@@ -11,6 +12,9 @@ app.engine(
 app.set('view engine', '.hbs');
 
 app.use(express.static(path.join(__dirname, '/public')));
+
+app.use(formidableMiddleware());
+// app.use(express.urlencoded({ extended: false }));
 
 app.get('/hello/:name', (req, res) => {
   res.render('hello', { layout: false, name: req.params.name });
@@ -34,6 +38,29 @@ app.get('/info', (req, res) => {
 
 app.get('/history', (req, res) => {
   res.render('history', { layout: 'dark' });
+});
+
+app.post('/contact/send-message', (req, res) => {
+  const fileType = accepts(req).types(['.png', '.jpg', '.jpeg', '.gif']);
+  const { author, sender, title, message } = req.fields;
+  const { projectFile } = req.files;
+
+  if (fileType && author && sender && title && message && projectFile) {
+    const ext = path.extname(projectFile.name);
+    if (['.png', '.jpg', '.jpeg', '.gif'].includes(ext)) {
+      res.render('contact', { isSent: true, file: projectFile.name });
+    } else {
+      res.render('contact', {
+        isError: true,
+        message: 'File type not accepted',
+      });
+    }
+  } else {
+    res.render('contact', {
+      isError: true,
+      message: "You can't leave fields empty!",
+    });
+  }
 });
 
 app.use((req, res) => {
